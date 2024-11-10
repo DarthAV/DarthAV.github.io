33ed2c542bff592b23d929956a05d37f07336afa
Ansh Verma <ansh.verma22@outlook.com>
2024-11-10 13:13:34 -0600

    animation scroll for projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 const { nextui } = require("@nextui-org/react");
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -6,10 +9,21 @@ module.exports = {
     './src/**/*.{html,js,ts,jsx,tsx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
-  theme: {
-    extend: {},
-  },
   darkMode: "class",
+  theme: {
+    extend: {
+      animation: {
+        scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
+    },
+  },
   plugins: [
     nextui({
       addCommonColors: true,
